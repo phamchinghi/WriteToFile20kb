86e02716eec59f98316cea7fd570246be7727672
Phạm Chí Nghị <phamchinghi11@gmail.com>
2021-08-15 18:44:40 +0700

    use another way not (short)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
+import com.sun.jdi.ShortType;
+
 import java.io.*;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.lang.Integer;
-import java.util.Base64;
 
 public class Main {
 
@@ -16,20 +11,39 @@ public class Main {
 
 
 //==============================Write to file  usage=============================
-        DataOutputStream out = new DataOutputStream(new FileOutputStream("D:\\mydata.txt"));
+        DataOutputStream out = new DataOutputStream(new FileOutputStream("D:\\mydata2.txt"));
+        byte[] buf = new byte[2];
         for (int i = 50000; i <= 60000; i++) {
-//           out.write(i); //10kb
-//           out.writeInt(i); //40kb
-            out.writeShort(i); //20kb
+            //=====Cách 1
+//            buf[0] = (byte)(i >> 8);
+//            buf[1] = (byte)(i & 0xFF);
+//            out.write(buf);
+            //=====Cách 2
+            ByteBuffer dbuf = ByteBuffer.allocate(2);
+            dbuf.putShort((short) i);
+            byte[] bytes = dbuf.array();
+            out.writeByte(bytes[0]);
+            out.writeByte(bytes[1]);
         }
+        out.flush();
         out.close();
 //===============================Read file  usage========================
-        DataInputStream in = new DataInputStream(new FileInputStream("D:\\mydata.txt"));
-        while (in.available() > 0) {
-//            System.out.println(in.readInt());
-            System.out.println(in.readUnsignedShort());
+        DataInputStream in = new DataInputStream(new FileInputStream("D:\\mydata2.dat"));
+        byte[] buf1 = new byte[2];// tạo mảng ký tự chứa 2 byte đọc ra
+
+        while (in.available() > 0)
+        {
+            buf1[0] = in.readByte();//bỏ byte đầu vào
+            buf1[1] = in.readByte();//bỏ byte sau vào
+
+            ByteBuffer wrapped = ByteBuffer.wrap(buf1);
+            int num = Short.toUnsignedInt(wrapped.getShort());
+            System.out.print(num +" ");
         }
         in.close();
+
+
+
     }
 
 }
