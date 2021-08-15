1e7078754566102f701584490822aee64e0fd6ca
Phạm Chí Nghị <phamchinghi11@gmail.com>
2021-08-15 19:07:05 +0700

    Sua extension file & thêm ghi chú

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 
 //==============================Write to file  usage=============================
-        DataOutputStream out = new DataOutputStream(new FileOutputStream("D:\\mydata2.txt"));
+        DataOutputStream out = new DataOutputStream(new FileOutputStream("D:\\mydata2.dat"));
         byte[] buf = new byte[2];
         for (int i = 50000; i <= 60000; i++) {
             //=====Cách 1
@@ -19,11 +19,12 @@ public class Main {
 //            buf[1] = (byte)(i & 0xFF);
 //            out.write(buf);
             //=====Cách 2
-            ByteBuffer dbuf = ByteBuffer.allocate(2);
-            dbuf.putShort((short) i);
-            byte[] bytes = dbuf.array();
-            out.writeByte(bytes[0]);
+            ByteBuffer dbuf = ByteBuffer.allocate(2);// tọa 1 bộ đệm mới gồm 2 byte
+            dbuf.putShort((short) i);//ghi gtri i vào mảng buf theo kiểu short
+            byte[] bytes = dbuf.array();//.array trả về mảng byte
+            out.writeByte(bytes[0]);//ghi xuống 1 byte -> ghi vào mảng buf
             out.writeByte(bytes[1]);
+            System.out.println("Write done!");
         }
         out.flush();
         out.close();
@@ -36,14 +37,11 @@ public class Main {
             buf1[0] = in.readByte();//bỏ byte đầu vào
             buf1[1] = in.readByte();//bỏ byte sau vào
 
-            ByteBuffer wrapped = ByteBuffer.wrap(buf1);
+            ByteBuffer wrapped = ByteBuffer.wrap(buf1);//gói mảng byte đó vào bộ đệm
             int num = Short.toUnsignedInt(wrapped.getShort());
             System.out.print(num +" ");
         }
         in.close();
-
-
-
     }
 
 }
