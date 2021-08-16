45825b03dd1cfe23c06a80e19842c33f2b4e0476
Phạm Chí Nghị <phamchinghi11@gmail.com>
2021-08-16 10:13:56 +0700

    update don't use any short word

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,64 @@ import java.io.*;
 import java.nio.ByteBuffer;
 
 public class Main {
-
-
-
     public static void main(String[] args) throws Exception {
-
-
-//==============================Write to file  usage=============================
-        DataOutputStream out = new DataOutputStream(new FileOutputStream("D:\\mydata2.dat"));
-        byte[] buf = new byte[2];
-        for (int i = 50000; i <= 60000; i++) {
-            //=====Cách 1
-//            buf[0] = (byte)(i >> 8);
-//            buf[1] = (byte)(i & 0xFF);
-//            out.write(buf);
-            //=====Cách 2
-            ByteBuffer dbuf = ByteBuffer.allocate(2);// tọa 1 bộ đệm mới gồm 2 byte
-            dbuf.putShort((short) i);//ghi gtri i vào mảng buf theo kiểu short
-            byte[] bytes = dbuf.array();//.array trả về mảng byte
-            out.writeByte(bytes[0]);//ghi xuống 1 byte -> ghi vào mảng buf
-            out.writeByte(bytes[1]);
-            System.out.println("Write done!");
-        }
-        out.flush();
-        out.close();
+//==============================Write to file  usage=====================
+//        write2Bytetofile("D:\\mydata2.dat");
 //===============================Read file  usage========================
-        DataInputStream in = new DataInputStream(new FileInputStream("D:\\mydata2.dat"));
-        byte[] buf1 = new byte[2];// tạo mảng ký tự chứa 2 byte đọc ra
-
-        while (in.available() > 0)
-        {
-            buf1[0] = in.readByte();//bỏ byte đầu vào
-            buf1[1] = in.readByte();//bỏ byte sau vào
-
-            ByteBuffer wrapped = ByteBuffer.wrap(buf1);//gói mảng byte đó vào bộ đệm
-            int num = Short.toUnsignedInt(wrapped.getShort());
-            System.out.print(num +" ");
-        }
-        in.close();
+        read2Bytefromfile("D:\\mydata2.dat");
     }
 
+
+    private static void write2Bytetofile(String fileName) throws FileNotFoundException {
+        try {
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(fileName));
+            byte[] buf = new byte[2];
+            for (int i = 50000; i <= 60000; i++) {
+//---------------------------------------------------Cách đầu tiên
+                //out.writeShort(i); //20kb
+
+//----------------------------------------------Cách 2---- không được dùng bất cứ từ short nào
+//                ByteBuffer dbuf = ByteBuffer.allocate(2);// tạo 1 bộ đệm mới gồm 2 byte
+//                dbuf.putShort((short) i);//ghi gtri i vào mảng buf theo kiểu short
+//                byte[] bytes = dbuf.array();//.array trả về mảng byte
+//                out.writeByte(bytes[0]);//ghi xuống 1 byte -> ghi vào mảng buf
+//                out.writeByte(bytes[1]);
+
+//-----------------------------------------------Cách 3---- dùng được
+
+                buf[0] = (byte)(i >> 8);
+                buf[1] = (byte)(i & 0xFF);
+                out.write(buf);
+            }
+            out.flush();
+            out.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+    private static void read2Bytefromfile(String fileName) throws FileNotFoundException {
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(fileName));
+            byte[] buf1 = new byte[2];// tạo mảng ký tự chứa 2 byte đọc ra
+
+            while (in.available() > 0)
+            {
+//--------------------------Đọc theo cách dùng được
+                buf1[0] = in.readByte();//bỏ byte đầu vào
+                buf1[1] = in.readByte();//bỏ byte sau vào
+                int num = ((buf1[0] & 0xff) << 8) | (buf1[1] & 0xff);
+                System.out.print(num + " ");
+
+//--------------------------Đọc theo cách 2
+//            ByteBuffer wrapped = ByteBuffer.wrap(buf1);//gói mảng byte đó vào bộ đệm
+//            int num = Short.toUnsignedInt(wrapped.getShort());
+//            System.out.print(num +" ");
+
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
